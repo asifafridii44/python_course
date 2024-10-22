737a1f76d1a26b1cb2bda1aa1a37d4a15afdc096
MUHAMMAD ASIF <140275007+asifafridii44@users.noreply.github.com>
2024-10-23 01:00:17 +0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+# this is first python program
+print("HELLO WORLD ")
+#The print() function is used to display output to the console.
+#Inside the parentheses, you have a string "HELLO WORLD " that will be printed when the program is run.
+#The string "HELLO WORLD " is the output that will appear on the screen
